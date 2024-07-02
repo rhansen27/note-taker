bc2d1d6b672bd295c9641fff82817b86ae27233c
rph <rhansen0127@gmail.com>
2024-07-02 00:22:44 -0500

    saving progresss on routes

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ const { readNotedDB, writeNotesDB } = require("./utils/write-file");
 
 app.use(express.static("public"));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
